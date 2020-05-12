7ad463afcab8a896ae99409faade1a97c51b6fcc
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2020-05-12 17:24:54 +0200

    Combine solver classes for now.

--- simple.py
+++ simple.py
@@ -1,22 +1,112 @@
+from collections import Counter
 from string import ascii_lowercase
 
 import numpy as np
-from consts import STANDARD_ALPHABET_SIZE
-from solver import AbstractSolver
+from consts import (
+    DIGRAM_FREQS_ENGLISH,
+    ENGLISH_LETTERS_BY_FREQUENCY,
+    STANDARD_ALPHABET_SIZE,
+)
 
 
-class SimpleSolver(AbstractSolver):
-    """Solver for simple monoalphabetic and polyalphabetic substitution ciphers."""
+class SimpleSolver:
+    """Solver for simple monoalphabetic substitution ciphers."""
 
     def __init__(self, ciphertext):
+        """Create new solver.
+
+        This creates a new cipher solver from an initial ciphertext.
+
+        Parameters
+        ----------
+        ciphertext : str
+            The ciphertext to solve.
+
+        Raises
+        ------
+        ValueError
+            If the passed ciphertext is not a string, or is empty.
+        """
+
+        if not isinstance(ciphertext, str):
+            raise ValueError(f"{ciphertext} is not a string.")
+
+        if len(ciphertext) < 1:
+            raise ValueError("Ciphertext cannot be empty.")
+
         # The decryption key is a string of letters that determines how the ciphertext
         # is converted to plaintext. The key is equal in length to the English alphabet
         # and is assumed to be in frequency order. In other words, the first letter is
         # the letter in the ciphertext that should be translated to an "e", the second
-        # which one should be converted to a "t", and so on.
+        # which one should be converted to a "t", and so on. When generating plaintext
+        # this key will be converted to an alphabetical one that matches the order of
+        # English alphabet which is the common way to express decryption keys.
         self._decryption_key = self._get_initial_key(ciphertext)
 
-        super().__init__(ciphertext)
+        self._ciphertext = ciphertext.lower()
+
+    def _score(self, matrix1, matrix2=DIGRAM_FREQS_ENGLISH):
+        if matrix1.shape != matrix2.shape:
+            raise ValueError("Digram frequency matrices must have the same dimensions")
+
+        return abs(matrix1 - matrix2).sum()
+
+    def _get_common_letters(self, text):
+        c = Counter(text)
+        return "".join([letter[0] for letter in c.most_common()])
+
+    def _get_digram_frequencies(self, text, alphabet_size=STANDARD_ALPHABET_SIZE):
+        """Generate digram frequencies for the passed text.
+
+        Parameters
+        ----------
+        text : str
+            Text to generate digram frequencies for.
+
+        Returns
+        -------
+        digram_frequencies : numpy.array
+            An array of digram frequencies indexed by [first][second] letter.
+        """
+
+        frequencies = np.zeros((alphabet_size, alphabet_size))
+
+        text = text.lower()
+        text_length = len(text)
+
+        for i in range(0, text_length - 1):
+            a = ascii_lowercase.index(text[i])
+            b = ascii_lowercase.index(text[i + 1])
+            frequencies[a, b] += 1
+
+        # Replace each entry with a percentage of the total text length, to get the same
+        # format as the English digram frequencies.
+        rows, columns = frequencies.shape
+        for i in range(rows):
+            for j in range(columns):
+                frequencies[i, j] = 100 * frequencies[i, j] / text_length
+
+        return frequencies
+
+    def _common_to_alphabetical_key(self, key):
+        alphabetical_key = [""] * STANDARD_ALPHABET_SIZE
+
+        for key_letter, english_letter in zip(key, ENGLISH_LETTERS_BY_FREQUENCY):
+            index = ascii_lowercase.index(english_letter)
+            alphabetical_key[index] = key_letter
+
+        return "".join(alphabetical_key)
+
+    def _get_plaintext(self, decryption_key):
+        if len(set(decryption_key)) != STANDARD_ALPHABET_SIZE:
+            raise ValueError(f"Key must include all letters of the alphabet.")
+
+        translation_table = {}
+
+        for cipher_letter, plain_letter in zip(decryption_key, ascii_lowercase):
+            translation_table[cipher_letter] = plain_letter
+
+        return "".join([translation_table[c] for c in self._ciphertext])
 
     def _get_initial_key(self, ciphertext):
         """Construct the initial decryption key.
@@ -63,10 +153,13 @@ class SimpleSolver(AbstractSolver):
         # We need this as a list so we can modify it in-place.
         key = [c for c in self._decryption_key]
 
-        print(f"starting with key = {key}")
+        print(f"starting with key = {''.join(key)}")
+        print(
+            f"starting with alphabetical key = {self._common_to_alphabetical_key(''.join(key))}"
+        )
 
         # Generate digram frequencies from the corresponding plaintext.
-        putative_plaintext = self._get_plaintext(key)
+        putative_plaintext = self._get_plaintext(self._common_to_alphabetical_key(key))
         digram_frequencies = self._get_digram_frequencies(putative_plaintext)
 
         # Calculate initial score.
@@ -95,9 +188,14 @@ class SimpleSolver(AbstractSolver):
 
         self._decryption_key = "".join(key)
         print(f"final key: {self._decryption_key}")
+        print(
+            f"final alphabetical key: {self._common_to_alphabetical_key(self._decryption_key)}"
+        )
 
     def plaintext(self):
-        return self._get_plaintext(self._decryption_key)
+        alphabetical_key = self._common_to_alphabetical_key(self._decryption_key)
+
+        return self._get_plaintext(alphabetical_key)
 
     def reset(self):
         self._decryption_key = self._get_initial_key(self._ciphertext)
