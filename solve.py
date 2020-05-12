bf82a952b92821089b029f8a0f4f9d5bed42accd
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2020-05-12 14:37:06 +0200

    Add simple CLI.

---
+++ solve.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# solve.py [--type=simple] <path_to_ciphertext_file>
+
+import sys
+
+from simple import SimpleSolver
+
+SOLVERS = {
+    "simple": SimpleSolver,
+}
+
+cipher_type = "simple"
+
+for arg in sys.argv:
+    if "--type" in arg:
+        _, cipher_type = arg.split("=")
+
+if cipher_type not in SOLVERS:
+    sys.exit(f'Unknown cipher type "{cipher_type}"')
+
+solver_class = SOLVERS[cipher_type]
+
+input_file = sys.argv[-1]
+
+with open(input_file) as f:
+    ciphertext = f.read().strip()
+
+s = solver_class(ciphertext)
+
+s.solve()
+
+print(s.plaintext())
