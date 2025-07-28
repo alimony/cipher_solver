33be0ae98235ba39c235f541772201e1d2db2213
Markus Amalthea Magnuson <markus@polyscopic.works>
2025-07-28 10:29:16 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="cipher_solver",
-    version="1.0.0",
+    version="1.0.2",
     description="Algorithm for solving simple, monoalphabetic substitution ciphers",
     long_description=long_description,
     long_description_content_type="text/markdown",
